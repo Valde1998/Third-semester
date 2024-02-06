25b409f7f801b0c289dfbf234f9fcd8ab8f06cd2
Valde1998 <124771370+Valde1998@users.noreply.github.com>
2024-02-06 17:56:21 +0100

    Opg2

---
+++ week2/Opg2.java
@@ -0,0 +1,55 @@
+public class Opg2 {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                counter.increment();
+                System.out.println("Thread 1: Count = " + counter.getCount());
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                counter.increment();
+                System.out.println("Thread 2: Count = " + counter.getCount());
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        thread1.start();
+        thread2.start();
+
+        try {
+
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Final count: " + counter.getCount());
+    }
+
+    private static class Counter {
+        private int count = 0;
+
+        public synchronized void increment() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+}
