65d648bc16653a612d54bb80334b5d3eba588dfa
Valde1998 <124771370+Valde1998@users.noreply.github.com>
2024-02-07 00:31:01 +0100

    JasonDTOExercise

---
+++ week2/JsonDTOExercise.java
@@ -0,0 +1,34 @@
+/*
+1: Json stand for JavaScript Object Notation
+
+2: JSON doesn't use end tag
+ JSON is shorter
+ JSON is quicker to read and write
+ JSON can use arrays
+ The biggest difference is: XML has to be parsed with an XML parser. JSON can be parsed by a standard JavaScript function.
+
+3: JSON is generally used for exchanging and storing structured data between a server and a web application.
+
+4:The 6 data types in JSON:
+1 String: A sequence of characters enclosed in double quotes, e.g., "hello world".
+
+2 Number: A numerical value, which can be an integer or a floating-point number, e.g., 42 or 3.14.
+
+3 Boolean: A value representing true or false.
+
+4 Array: An ordered collection of values, enclosed in square brackets [], e.g., [1, 2, 3].
+
+5 Object: An unordered collection of key-value pairs, enclosed in curly braces {}, e.g., {"name": "John", "age": 30}.
+
+6 Null: A special value representing null or empty, used to indicate the absence of a value.
+
+ 5: Write down the 4 JSON syntax rules.
+Data is in name/value pairs.
+Data is separated by commas.
+Curly braces hold objects.
+Square brackets hold arrays.
+
+ */
+public class JsonDTOExercise {
+
+}
