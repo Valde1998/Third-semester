95f994c0261bf745ce7d84511a52efea71e7c8bf
Valdemar <valde.dk@hotmail.com>
2024-02-07 15:27:45 +0100

    y

--- week2/Opg2.java
+++ week2/Opg2.java
@@ -2,7 +2,7 @@ public class Opg2 {
     public static void main(String[] args) {
         Counter counter = new Counter();
         Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 1000; i++) {
                 counter.increment();
                 System.out.println("Thread 1: Count = " + counter.getCount());
                 try {
@@ -14,11 +14,11 @@ public class Opg2 {
         });
 
         Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 1000; i++) {
                 counter.increment();
                 System.out.println("Thread 2: Count = " + counter.getCount());
                 try {
-                    Thread.sleep(50);
+                    Thread.sleep(10);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
