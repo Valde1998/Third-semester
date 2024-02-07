0e9d74c09764f3dbaff24d0006ea51ec3d3c3110
Valdemar <valde.dk@hotmail.com>
2024-02-07 03:09:06 +0100

    week2

--- week2/account/json.kt
+++ week2/account/json.kt
@@ -1,19 +1,1 @@
-package account
-    [
-    {
-        "firstName": "John",
-        "lastName": "Doe",
-        "birthDate": "1990-01-01",
-        "address": {
-        "street": "123 Main St",
-        "city": "Cityville",
-        "zipCode": 12345
-    },
-        "account": {
-        "id": "123456789",
-        "balance": "1000.00",
-        "isActive": true
-    }
-    },
-    ]
 
