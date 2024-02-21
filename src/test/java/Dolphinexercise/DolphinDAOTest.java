720a0c16dc85e0eb8e26ef146b021dcaf3d3138e
Valdemar <valde.dk@hotmail.com>
2024-02-21 14:20:57 +0100

    Create DolphinDAOTest.java

---
+++ src/test/java/Dolphinexercise/DolphinDAOTest.java
@@ -0,0 +1,53 @@
+package Dolphinexercise;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DolphinDAOTest {
+
+    private static EntityManagerFactory emfTest;
+
+    private  static  DolphinDAO dolphinDAO;
+
+    private int[] IDS;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createPerson() {
+    }
+
+    @Test
+    void readPerson() {
+    }
+
+    @Test
+    void getPersonNotes() {
+    }
+
+    @Test
+    void getPersonTotalAmountPaid() {
+    }
+
+    @Test
+    void updatePerson() {
+    }
+
+    @Test
+    void deletePerson() {
+    }
+
+    @Test
+    void readAllPersonsDTO() {
+    }
+}
