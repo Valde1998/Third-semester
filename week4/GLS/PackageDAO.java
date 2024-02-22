6a4a72d6354586ea324f776b29f0a11886206448
Valde1998 <124771370+Valde1998@users.noreply.github.com>
2024-02-22 15:05:18 +0100

    GLS

---
+++ week4/GLS/PackageDAO.java
@@ -0,0 +1,14 @@
+package GLS;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+
+import java.util.List;
+
+public class PackageDAO {
+    private EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryConfig();
+
+}
+
+
